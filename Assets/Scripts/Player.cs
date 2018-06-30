f5dd40047f03bd23c2afa92194273b34388ca0bf
aryyya <arya@live.ca>
2018-06-29 22:54:00 -0700

    Add keyboard movement.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed = 250.0f;
+
+    private Rigidbody2D _body;
+
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+
+        Vector2 movement = new Vector2(deltaX, _body.velocity.y);
+        _body.velocity = movement;
+    }
+}
