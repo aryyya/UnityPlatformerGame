44cbb5f0d598ef72dedbe7c7f883e5079852a88e
aryyya <arya@live.ca>
2018-06-29 23:53:54 -0700

    Add walking animations.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Inspector fields.
     [SerializeField] private float _speed = 250.0f;
 
+    // Components.
     private Rigidbody2D _body;
+    private Animator _animator;
 
     void Start()
     {
+        // Components.
         _body = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        // Input.
         float deltaX = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
 
+        // Velocity.
         Vector2 movement = new Vector2(deltaX, _body.velocity.y);
         _body.velocity = movement;
+
+        // Animation.
+        _animator.SetFloat("speed", Mathf.Abs(deltaX));
+        if (!Mathf.Approximately(deltaX, 0))
+        {
+            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
+        }
     }
 }
