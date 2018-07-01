e150e2795df8121d96a0733d3121d66e8e207764
aryyya <arya@live.ca>
2018-06-30 18:44:41 -0700

    Only jump when on the ground.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,14 +9,16 @@ public class Player : MonoBehaviour
     [SerializeField] private float _jumpForce = 12.0f;
 
     // Components.
-    private Rigidbody2D _body;
+    private Rigidbody2D _rigidbody;
     private Animator _animator;
+    private BoxCollider2D _boxCollider;
 
     void Start()
     {
         // Components.
-        _body = GetComponent<Rigidbody2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        _boxCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update()
@@ -25,13 +27,20 @@ public class Player : MonoBehaviour
         float deltaX = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
 
         // Horizontal movement.
-        Vector2 movement = new Vector2(deltaX, _body.velocity.y);
-        _body.velocity = movement;
+        Vector2 movement = new Vector2(deltaX, _rigidbody.velocity.y);
+        _rigidbody.velocity = movement;
 
         // Jump movement.
-        if (Input.GetKeyDown(KeyCode.Space))
+        Vector3 max = _boxCollider.bounds.max;
+        Vector3 min = _boxCollider.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - 0.1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - 0.2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+        bool isGrounded = hit != null;
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
-            _body.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
         }
 
         // Animation.
