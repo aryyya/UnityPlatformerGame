d1affa051af6014dbf3638ade9a46c0c6783e2c5
aryyya <arya@live.ca>
2018-07-02 15:59:03 -0700

    Add smooth follow camera.

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _smoothTime = 0.2f;
+
+    private Vector3 _velocity = Vector3.zero;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    void LateUpdate()
+    {
+        Vector3 targetPosition = new Vector3(_target.position.x, _target.position.y, _target.position.y);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, _smoothTime);
+    }
+}
