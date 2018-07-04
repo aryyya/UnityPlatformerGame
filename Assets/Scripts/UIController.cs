b3b686be200c027afebab3404a4dc426c39c7a2d
aryyya <arya@live.ca>
2018-07-04 16:47:57 -0700

    Add UI controller.

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    // Inspector.
+    [SerializeField] private Text _scoreLabel;
+
+    private void Update()
+    {
+        _scoreLabel.text = Time.realtimeSinceStartup.ToString();
+    }
+
+    public void OnOpenSettings()
+    {
+        Debug.Log("Open Settings");
+    }
+}
