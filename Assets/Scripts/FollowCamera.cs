d2760e6a43cc82a9c5043ed085b699f91ffafd2a
aryyya <arya@live.ca>
2018-07-04 15:44:00 -0700

    Remove some unnecessary code.

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
+    // Inspector fields.
     [SerializeField] private Transform _target;
     [SerializeField] private float _smoothTime = 0.2f;
 
     private Vector3 _velocity = Vector3.zero;
 
-    void Start()
-    {
-    }
-
-    void Update()
-    {
-    }
-
     void LateUpdate()
     {
+        // Change to target position with a smooth delay.
         Vector3 targetPosition = new Vector3(_target.position.x, _target.position.y, transform.position.z);
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, _smoothTime);
     }
